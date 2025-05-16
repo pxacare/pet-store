3cc322d8da9d2246d71b020458df0ecc44d44f96
Ponchanon Datta Rone <ponchanon.rone@gmail.com>
2025-05-16 11:47:45 -0400

    Local build successful

--- pet-store/src/app.module.ts
+++ pet-store/src/app.module.ts
@@ -8,10 +8,13 @@ import { AppLoggerMiddleware } from './HttpLogger.middleware';
 import { PetModule } from './pet/pet.module';
 
 export const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+// export const MONGO_HOST = 'localhost';
+
 export const appModuleMetadata: ModuleMetadata = {
   imports: [
     PetModule,
-    MongooseModule.forRoot(`mongodb://pet-store-app:pet-store-app-pw@${MONGO_HOST}:27017/pet-store?authSource=pet-store`),
+    // MongooseModule.forRoot(`mongodb://pet-store-app:pet-store-app-pw@${MONGO_HOST}:27017/pet-store?authSource=pet-store`),
+    MongooseModule.forRoot(`mongodb://pet-store-app:pet-store-app-pw@pet-store-mongo-1:27017/pet-store?authSource=pet-store`),
   ],
   controllers: [],
   providers: [],
